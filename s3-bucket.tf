332c24dad2d346340e439853e9b2fbf78fb9d41e
ravihraj2017 <142050519+ravihraj2017@users.noreply.github.com>
2023-08-24 13:30:49 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/ravihraj_terraform_amazon/s3-bucket/aws"
-  bucket_prefix = "my-s3-bucket-ravi"
+  bucket_prefix = "ravihamsarajukumars3"
   version = "2.8.0"
 }
