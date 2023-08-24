70bc0cb0180b63ed00e4bee7d86a169fcdfd72ae
ravihraj2017 <142050519+ravihraj2017@users.noreply.github.com>
2023-08-24 13:08:48 -0500

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,15 +1,5 @@
-module "s3_bucket" {
+module "s3-bucket" {
   source  = "app.terraform.io/ravihraj_terraform_amazon/s3-bucket/aws"
   version = "2.8.0"
-
-  bucket_prefix = "my-s3-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+  bucket_prefix = "my-ravi-bucket" 
 }
-
